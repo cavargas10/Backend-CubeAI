c1e6034dc61aa94892924c12113b5a54865198f6
Carlos Vargas R. <carvarjr_95@hotmail.com>
2024-07-06 02:50:55 -0500

    Cambios en Unico3D

--- services/unico3d_service.py
+++ services/unico3d_service.py
@@ -8,14 +8,16 @@ from utils.storage_utils import upload_to_storage
 
 load_dotenv()
 
-client = Client("cavargas10/Unico3D")
+client_unico3d_url = os.getenv("CLIENT_UNICO3D_URL")
+
+client = Client(client_unico3d_url)
 
 def unico3d_generation_exists(user_uid, generation_name):
     doc_ref = db.collection('predictions').document(user_uid).collection('Unico3D').document(generation_name)
     doc = doc_ref.get()
     return doc.exists
 
-def create_unico3d_generation(user_uid, image_file, generation_name):
+def create_unico3d(user_uid, image_file, generation_name):
     if unico3d_generation_exists(user_uid, generation_name):
         raise ValueError("El nombre de la generación ya existe. Por favor, elige otro nombre.")
 
@@ -23,7 +25,7 @@ def create_unico3d_generation(user_uid, image_file, generation_name):
     image_file.save(unique_filename)
 
     try:
-        result = client.predict(
+        result_generate3dv2 = client.predict(
             preview_img=file(unique_filename),
             input_processing=True,
             seed=-1,
@@ -34,34 +36,26 @@ def create_unico3d_generation(user_uid, image_file, generation_name):
             api_name="/generate3dv2"
         )
 
-        # Imprimir el resultado para depuración
-        print(f"Resultado de la predicción: {result}")
-
-        # Asumiendo que el resultado es una tupla con la ruta del archivo .glb como primer elemento
-        if isinstance(result, tuple) and len(result) > 0:
-            glb_path = result[0]
+        if isinstance(result_generate3dv2, tuple):
+            obj_glb_path = result_generate3dv2[0]  
         else:
-            raise ValueError(f"Resultado inesperado de la predicción: {result}")
-
-        # Verificar si glb_path es una ruta válida
-        if not isinstance(glb_path, (str, bytes, os.PathLike)):
-            raise ValueError(f"La ruta del archivo GLB no es válida: {glb_path}")
+            obj_glb_path = result_generate3dv2
 
         generation_folder = f'{user_uid}/{generation_name}'
-        glb_url = upload_to_storage(glb_path, f'{generation_folder}/model.glb')
+
+        obj_glb_url = upload_to_storage(obj_glb_path, f'{generation_folder}/obj_glb.glb')
 
         prediction_result = {
             "generation_name": generation_name,
-            "glb_model": glb_url,
+            "obj_glb": obj_glb_url,
             "timestamp": datetime.datetime.now().isoformat(),
-            "prediction_type": "Unico3D"
+            "prediction_type": "Unico a 3D"
         }
 
         doc_ref = db.collection('predictions').document(user_uid).collection('Unico3D').document(generation_name)
         doc_ref.set(prediction_result)
 
         return prediction_result
-
     finally:
         if os.path.exists(unique_filename):
             os.remove(unique_filename)
@@ -75,7 +69,7 @@ def delete_unico3d_generation(user_uid, generation_name):
     doc = doc_ref.get()
     if not doc.exists:
         return False
-
+    
     generation_folder = f"{user_uid}/{generation_name}"
     blobs = bucket.list_blobs(prefix=generation_folder)
     for blob in blobs:
